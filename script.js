c2d6c3ef9ceee2b2bba27e54ae44cc392b3d04ae
ShashiniWanninayake <shashinethma15@gmail.com>
2026-06-18 05:43:36 -0800

    script.js

--- script.js
+++ script.js
@@ -72,3 +72,9 @@ function animate() {
 }
 
 animate();
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
